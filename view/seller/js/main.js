dba22a04f8fc488dc4ad77ce8ab61e9ccdbc2595
SocheatLun <socheat.lun@student.passerellesnumeriques.org>
2022-11-30 12:47:02 +0700

    [CREATE_PRODUCT] create product page and store product on LocalStorage

--- view/seller/js/main.js
+++ view/seller/js/main.js
@@ -1,46 +1,139 @@
 /// DOM ELEMENTS///
-const product = document.querySelector("#dialog-cover");
+const showProduct = document.querySelector("#dialog-cover");
+const dom_product_view = document.querySelector(".table")
 const dialog = document.querySelector("#dialog");
 const cancel = document.querySelector("#cancel");
 const add = document.querySelector("#add");
 
+
+let products = [
+    {
+        name: "T-shirt",
+        price: 50,
+        currency: "$",
+        description: "Good for boy",
+        image: "../../../img/T-Shirt.png",
+        edit: "../../../img/edit.png",
+        delete: "../../../img/delete.png",
+    },
+    {
+        name: "Nike Shoes",
+        price: 500,
+        currency: "$",
+        description: "best shoes ever",
+        image: "../../../img/Nike Shoes.png",
+        edit: "../../../img/edit.png",
+        delete: "../../../img/delete.png",
+    },
+    {
+        name: "Watch",
+        price: 150,
+        currency: "$",
+        description: "Hight level Watch",
+        image: "../../../img/Watch.png",
+        edit: "../../../img/edit.png",
+        delete: "../../../img/delete.png",
+    },
+];
+
+
+
+function saveProduct() {
+    localStorage.setItem("products", JSON.stringify(products));
+}
+saveProduct()
+function loadProduct() {
+    let productStorage = JSON.parse(localStorage.getItem("products"));
+    if (productStorage !== null) {
+        products = productStorage;
+
+    }
+}
+loadProduct()
+
 function showDialog(element) {
-    product.style.display = "block";
+    showProduct.style.display = "block";
 }
 
 function hideDialog(element) {
-    product.style.display = "none";
+    showProduct.style.display = "none";
 }
 // hide(content)
 // show(content)
 function onAddProduct() {
-    showDialog(product);
+    showDialog(showProduct);
 }
 function onCancel(e) {
-    hideDialog(product);
+    hideDialog(showProduct);
 }
 
-function createProduct(){
-    let table = document.querySelector("tbody")
-    let listOfProduct = document.createElement("tr")
-    let productName = document.createElement("td")
-    productName.textContent = document.querySelector("#name").value;
-    let productPrice =  document.createElement("td")
-    productPrice.textContent = document.querySelector("#price").value;
-    let productDescription =  document.createElement("td")
-    productDescription.textContent = document.querySelector("#description").value;
-    let productCurrency =  document.createElement("td")
-    productCurrency.textContent = document.querySelector("#currency").value;
-    let productImage =  document.createElement("td")
-    productImage.textContent = document.querySelector("#image").value;
-    listOfProduct.appendChild(productName);
-    listOfProduct.appendChild(productPrice);
-    listOfProduct.appendChild(productDescription);
-    listOfProduct.appendChild(productCurrency);
-    listOfProduct.appendChild(productImage);
-    table.appendChild(listOfProduct);
-    console.log(table);
+function renderProduct() {
+    // Remove the card container and create a new one
+    let dom_product_container = document.querySelector("#product-container");
+    dom_product_container.remove();
+    dom_product_container = document.createElement("tbody");
+    dom_product_container.id = "questions-container";
+    dom_product_view.appendChild(dom_product_container);
+
+    // 2 - For all questions,  create a new div (class : item), and append it the container
+    for (let index = 0; index < products.length; index++) {
+        let productList = products[index];
+        // list item of product //
+        let card = document.createElement("tr");
+        card.className = "card";
+        card.dataset.index = index;
+        dom_product_container.appendChild(card);
+
+        // create name of product //
+        let name = document.createElement("td");
+        name.className = "name";
+        name.textContent = productList.name;
+        card.appendChild(name);
+
+        // create currency of product //
+        let currency = document.createElement("td");
+        currency.className = "currency";
+        currency.textContent = productList.currency;
+        card.appendChild(currency);
+
+        //  create price of product //
+        let price = document.createElement("td");
+        price.className = "price";
+        price.textContent = productList.price;
+        card.appendChild(price);
+
+        // create description of product //
+        let description = document.createElement("td");
+        description.className = "description";
+        description.textContent = productList.description;
+        card.appendChild(description);
+
+        // create image of product //
+        let image = document.createElement("td");
+        let img = document.createElement("img");
+        img.src = productList.image;
+        image.appendChild(img);
+        image.className = "image";
+        image.appendChild(img);
+        card.appendChild(image);
+
+        // create edit button of product //
+        let edit = document.createElement("td");
+        let edit_img = document.createElement("img");
+        edit_img.src = productList.edit;
+        edit.appendChild(edit_img);
+        edit.className = "edit-image";
+        edit.appendChild(edit_img);
+        card.appendChild(edit);
+
+        // create delete button of product //
+        let deleteIcon = document.createElement("td");
+        let delete_img = document.createElement("img");
+        delete_img.src = productList.delete;
+        deleteIcon.appendChild(delete_img);
+        deleteIcon.className = "edit-image";
+        deleteIcon.appendChild(delete_img);
+        card.appendChild( deleteIcon);
+    }
 }
-createProduct()
-
-
+renderProduct()
